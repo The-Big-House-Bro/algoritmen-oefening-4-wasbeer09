ee12cccc32d75d70ff168eaa3f8b5267b409b241
Lore Putteman <lore.putteman@leerling.athenea.be>
2025-03-25 09:18:13 +0100

    huiswerk

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,12 @@
 def countTargetPairs():
+    count = 0
+    n= len(nums)
+    for i in range(n) :
+        for j in range(i+1) :
+            if nums[i] and nums[j] < target:
+                count += 1
+    return count
+nums = [1,2,3,4,5]
+target= 6
+print(countTargetPairs(nums,target))
+
